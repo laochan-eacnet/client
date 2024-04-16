bba42e4068d07c3fba90bc8a9dd6201c561e53ea
GEEKiDoS <geek_ds@foxmail.com>
2024-04-16 16:48:20 +0800

    asio device name selector

--- src/client/launcher/launcher.cpp
+++ src/client/launcher/launcher.cpp
@@ -28,104 +28,129 @@ void launcher::create_main_menu()
 	_main_window->set_title("Laochan-Eacnet Infinitas Launcher");
 	_main_window->set_size(750, 480, WEBVIEW_HINT_NONE);
 
+	_main_window->bind("getAsioDeviceList", [this](auto)->std::string
+		{
+			HKEY key;
+			if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\ASIO", 0, KEY_READ, &key))
+				return "[]";
+
+			DWORD subkey_counts, subkey_name_maxlen;
+			RegQueryInfoKeyA(key, nullptr, nullptr, nullptr, &subkey_counts, &subkey_name_maxlen, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+
+			auto result = json::array();
+
+			for (int i = 0; i < subkey_counts; i++)
+			{
+				std::string name;
+				DWORD size = subkey_name_maxlen;
+				name.resize(subkey_name_maxlen + 1);
+				RegEnumKeyExA(key, i, name.data(), &size, nullptr, nullptr, nullptr, nullptr);
+
+				name.resize(size);
+				result.push_back(name);
+			}
+
+			return result.dump();
+		});
+
 	_main_window->bind("getVersion", [this](auto)->std::string
-	{
-		return json{ VERSION }.dump();
-	});
+		{
+			return json{ VERSION }.dump();
+		});
 
 	_main_window->bind("startGame", [this](auto) -> std::string
-	{
-		run_game = true;
+		{
+			run_game = true;
 
-		_main_window->terminate();
-		return {};
-	});
-
-	_main_window->bind("showWindow", [this](auto) -> std::string 
-	{
-		std::thread{
-			&launcher::wait_and_show_window,
-			HWND(_main_window->window().value()), 16ms
-		}.detach();
-
-		return {};
-	});
-
-	_main_window->bind("hideWindow", [this](auto) -> std::string 
-	{
-		ShowWindow(reinterpret_cast<HWND>(_main_window->window().value()), SW_HIDE);
-		return {};
-	});
-
-	_main_window->bind("createSettingsProcess", [this](auto) -> std::string 
-	{
-		STARTUPINFO si = { 0 };
-		PROCESS_INFORMATION pi = { 0 };
-		si.cb = sizeof(si);
-		si.wShowWindow = SW_SHOW;
-
-		utils::nt::library bm2dx{};
-		auto settings_path = std::filesystem::path{
-			bm2dx.get_folder() + "/../../launcher/modules/bm2dx_settings.exe"
-		}.make_preferred().string();
-
-		CreateProcessA(
-			settings_path.data(), nullptr, 0, 0, false, 0,
-			nullptr, nullptr, &si, &pi
-		);
-
-		return {};
-	});
-
-	_main_window->bind("uuid", [this](auto) -> std::string 
-	{
-		const auto uuid = utils::string::dump_hex(utils::smbios::get_uuid(), "");
-		return json{ uuid }.dump();
-	});
-
-	_main_window->bind("getOptions", [this](auto) -> std::string 
-	{
-		if (!utils::io::file_exists("laochan-config.json")) 
-			return "null";
-
-		return utils::io::read_file("laochan-config.json");
-	});
-
-	_main_window->bind("setOptions", [this](auto param_json) -> std::string 
-	{
-		const json params = json::parse(param_json);
-
-		if (!params.is_array() || !params.size()) 
+			_main_window->terminate();
 			return {};
+		});
 
-		const auto options = params[0];
+	_main_window->bind("showWindow", [this](auto) -> std::string
+		{
+			std::thread{
+				&launcher::wait_and_show_window,
+				HWND(_main_window->window().value()), 16ms
+			}.detach();
 
-		if (!options.is_object()) 
 			return {};
+		});
 
-		if (options["token"].is_string())
-			launcher::token = options.value("token", "");
+	_main_window->bind("hideWindow", [this](auto) -> std::string
+		{
+			ShowWindow(reinterpret_cast<HWND>(_main_window->window().value()), SW_HIDE);
+			return {};
+		});
 
-		if (options["serverUrl"].is_string())
-			launcher::get_service_address = options.value("serverUrl", "");
+	_main_window->bind("createSettingsProcess", [this](auto) -> std::string
+		{
+			STARTUPINFO si = { 0 };
+			PROCESS_INFORMATION pi = { 0 };
+			si.cb = sizeof(si);
+			si.wShowWindow = SW_SHOW;
 
-		if (options["asioDevice"].is_string())
-			launcher::asio_device_name = options.value("asioDevice", "");
+			utils::nt::library bm2dx{};
+			auto settings_path = std::filesystem::path{
+				bm2dx.get_folder() + "/../../launcher/modules/bm2dx_settings.exe"
+			}.make_preferred().string();
 
-		if (options["displayMode"].is_number_integer())
-			launcher::disp_mode = options.value<launcher::display_mode>("displayMode", launcher::display_mode::windowed_720p);
+			CreateProcessA(
+				settings_path.data(), nullptr, 0, 0, false, 0,
+				nullptr, nullptr, &si, &pi
+			);
 
-		if (options["soundMode"].is_number_integer())
-			launcher::snd_mode = options.value<launcher::sound_mode>("soundMode", launcher::sound_mode::wasapi);
+			return {};
+		});
 
-		utils::io::write_file("laochan-config.json", options.dump());
-		return {};
-	});
+	_main_window->bind("uuid", [this](auto) -> std::string
+		{
+			const auto uuid = utils::string::dump_hex(utils::smbios::get_uuid(), "");
+			return json{ uuid }.dump();
+		});
+
+	_main_window->bind("getOptions", [this](auto) -> std::string
+		{
+			if (!utils::io::file_exists("laochan-config.json"))
+				return "null";
+
+			return utils::io::read_file("laochan-config.json");
+		});
+
+	_main_window->bind("setOptions", [this](auto param_json) -> std::string
+		{
+			const json params = json::parse(param_json);
+
+			if (!params.is_array() || !params.size())
+				return {};
+
+			const auto options = params[0];
+
+			if (!options.is_object())
+				return {};
+
+			if (options["token"].is_string())
+				launcher::token = options.value("token", "");
+
+			if (options["serverUrl"].is_string())
+				launcher::get_service_address = options.value("serverUrl", "");
+
+			if (options["asioDevice"].is_string())
+				launcher::asio_device_name = options.value("asioDevice", "");
+
+			if (options["displayMode"].is_number_integer())
+				launcher::disp_mode = options.value<launcher::display_mode>("displayMode", launcher::display_mode::windowed_720p);
+
+			if (options["soundMode"].is_number_integer())
+				launcher::snd_mode = options.value<launcher::sound_mode>("soundMode", launcher::sound_mode::wasapi);
+
+			utils::io::write_file("laochan-config.json", options.dump());
+			return {};
+		});
 
 	_main_window->set_html(load_content(MENU_MAIN));
 
 	std::thread{
-		&launcher::wait_and_show_window, 
+		&launcher::wait_and_show_window,
 		HWND(_main_window->window().value()), 5000ms
 	}.detach();
 }
