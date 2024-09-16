d8e1a86ab3cb5d3aaf11a4e75987e69963161228
GEEKiDoS <geek_ds@foxmail.com>
2024-09-16 13:56:10 +0800

    enable devtools on debug mode

--- src/client/launcher/launcher.cpp
+++ src/client/launcher/launcher.cpp
@@ -309,9 +309,12 @@ void launcher::create_main_menu()
 		}
 	);
 
-#if _DEBUG && !USE_EMBEDDED
-	smartview_->set_url("http://localhost:5173/");
+#if _DEBUG
 	smartview_->set_dev_tools(true);
+#endif
+	
+#if !USE_EMBEDDED
+	smartview_->set_url("http://localhost:5173/");	
 #else
 	smartview_->embed(laochan::embedded::all());
 	smartview_->serve("");
